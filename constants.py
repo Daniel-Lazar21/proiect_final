0cf5f1e5878a67382135015942472617f203532e
Daniel-Lazar21 <danutlazar128@gmail.com>
2023-12-26 23:30:00 +0200

    un mic update

--- constants.py
+++ constants.py
@@ -1,18 +1,42 @@
-import datetime
+import os
 # informatiile bazei de date 
 
-# ora la care se face update-ul parametrii fiind ( ora , minutul , secunda )
-ORA_UPDATE = datetime.time(20,0,0)
+# ora la care se face update-ul in formatul( "ora:minutul:secunda" ) ex : "20:00:00"
+"🢃"
+ORA_UPDATE = "20:00:00"
+"🢁"
 
 # numele user-ului
+"🢃"
 USER = "root"
+"🢁"
 
 # parola user-ului
-PASSWORD = r"Wer%^t123.:qwert44{Y}"
+if os.getlogin() == 'danut' :   
+    with open('psw.txt', 'r') as pswFile:
+        PASSWORD = pswFile.readline()
+else:
+    #❗️ Aici introduci tu parola ta❗️
+    
+    "🢃"
+    PASSWORD = "scrie_parola_ta" 
+    "🢁"
 
 # host-ul userului
+"🢃"
 HOST = "localhost"
+"🢁"
 
 # baza de date cu care se lucreaza 
+"🢃"
 DATABASE = "proiect_final"
+"🢁"
 
+# calea absoluta a directorului din care se iau fisierele csv sau txt
+"🢃"
+ABSOLUTE_PATH_INTRARI = r"C:\Users\danut\Desktop\pythonProject1\proiect_final\intrari"
+"🢁"
+
+# calea de backup se deduce automat din calea de intrari
+# adica iau calea de intrari pana la ultima aparitie a caracterului '\' si adaug 'backup_intrari' la acel string 
+ABSOLUTE_PATH_BACKUP_INTRARI = ABSOLUTE_PATH_INTRARI[ : ABSOLUTE_PATH_INTRARI.rindex('\\') + 1] + 'backup_intrari'
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from constants import * 
 import mysql.connector
-import os,time,sys,csv
+import os,time,sys,csv,datetime
 
 class ConnectionToDatabase():
+    """Cu ajutorl acestei clase instantiem conexiunea cu baza de date"""
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
@@ -32,26 +33,32 @@ class ConnectionToDatabase():
     def __repr__(self) :
         return f"Sunt o conexiune la baza de date {DATABASE} la adresa {hex(id(self))} !"
 
+class Person():
+    def __init__(self,nume,prenume,companie,id_manager,Email):
+        self.nume = nume
+        self.prenume = prenume
+        self.companie = companie
+        self.id_manager = id_manager
+        self.Email = Email
+     
+    
 myconnection = ConnectionToDatabase()
 
-PATH_TO_DIR = "proiect_final\intrari"#sys.argv[2] 
-
+# comenzile prin sys.argv : sys.argv[2] 
  
-def date_to_sql_format(date :str) -> str:
+def date_to_sql_format(date: str) -> str:
     """Transforma data din fisiere sub o forma accepatata de sql."""
-    sql_date = ''
-    for char in date:
-        if char == "T":
-            sql_date += " "
-        elif char == "Z":
-            sql_date += ""
-        else :
-            sql_date += char       
-    return sql_date[:-4]
+    sql_date = date.replace("T"," ")
+    sql_date = sql_date[:-5]     
+    return sql_date
             
-def add_to_database(file: str):
+def add_to_database(file: str, connection: ConnectionToDatabase):
     """Adauga informatiile dintr-un fisier csv sau txt in baza de date."""
-    file_type = file[file.index(".") + 1 :]
+    # din stringul file se ia doar ce se afla dupa caracterul'.' adica tipul fisierului 
+    # de exemplu din ...\Poarta2.csv se ia doar stringul 'csv'
+    file_type = file[file.index(".") + 1 : ]
+    # din stringul file se ia doar ce este inte ultimul caracter 'a'(cel dein cuvantul Poarta) pana la punct 
+    # de exemplu din ...\Poarta2.csv se ia doar cifra 2 si desigur se converteste la tipul int  
     gate = int(file[file.rindex('a') + 1 : file.index('.') ])
     
     if file_type == 'csv':
@@ -59,11 +66,10 @@ def add_to_database(file: str):
             content = csv.reader(csv_file)
             for index,line in enumerate(content):
                 if line[0].strip() != "":
-                    print(line)
-                    #adaug in baza de date inforamtiile si de la Sens sterg spatiile goale pentu a nu avea probleme mai tarziu
+                    #adaug in baza de date inforamtiile si de la Sens sterg spatiile goale pentu a nu avea probleme mai tarziu 🢃
                     if index != 0:
                         new_query = f"insert into acces values(null,{line[0]}, '{date_to_sql_format(line[1])}', '{line[2].strip()}', {gate})"
-                        myconnection.execute_insert_query(new_query)                     
+                        connection.execute_insert_query(new_query)                     
     elif file_type == 'txt':
         with open(file,'r') as txt_file:
             content = txt_file.readlines()
@@ -73,41 +79,74 @@ def add_to_database(file: str):
                     #sterg spatiile golale de la final ,iau randurile excluzand caracterele de la final(';' si '\n') si apoi dau split dupa ','
                     line = line.replace(" ","")[:-2].split(",")
                     #adaug in baza de date informatille
-                    print(line)
                     new_query = f"insert into acces values(null,{line[0]}, '{date_to_sql_format(line[1])}', '{line[2]}', {gate})"
-                    myconnection.execute_insert_query(new_query)
+                    connection.execute_insert_query(new_query)
+
+def calcul_ore_lucrate(connection : ConnectionToDatabase):
+    ids_persoane = connection.execute_select_query("select Id from persoane")
+    data_de_lucru = '2023-05-21'#datetime.datetime.now().strftime("%Y-%m-%d")
+    print(data_de_lucru)
+    print(ids_persoane)
+    for id_persoana in ids_persoane:
+        id_persoana = id_persoana[0]
+        date_persoana = myconnection.execute_select_query(f"select * from persoane where id = {id_persoana}")[0]
+        print(date_persoana)
+        #this_person = Person()
+        table_today = f"select * from acces where Data like '%{data_de_lucru}%' "
+        query_in = f"select * from ({table_today}) as today where Id_Persoana = {id_persoana} and Sens = 'in' "
+        persoana_first_entry = myconnection.execute_select_query(query_in)[0][2]
+        query_out = f"select * from ({table_today}) as today where Id_Persoana = {id_persoana} and Sens = 'out' "
+        persoana_last_exit = myconnection.execute_select_query(query_out)[-1][2]
+        
+        timp_la_lucru = str(persoana_last_exit - persoana_first_entry)
+        print(timp_la_lucru)
+        ore_lucrate = int(timp_la_lucru[0 : timp_la_lucru.index(":") ])
     
+        
+        if ore_lucrate < 8:
+            print("CHIULANGIU")
+        
     
-    
-def check_for_new_files():
-    """Verifica in fiecare secunda daca s-au adaugat fisiere noi in directorul dat"""
+  
+def update():
+    """Verifica in fiecare secunda daca s-au adaugat fisiere noi in directorul dat.
+    La ora 20:00:00 se face calculul de ore pe ziua respectiva.
+    """
  
-    lista = os.listdir(PATH_TO_DIR)
+    lista = os.listdir(ABSOLUTE_PATH_INTRARI)
     while True:
         changed = False
         elements_added = False
-        lista_noua = os.listdir(PATH_TO_DIR)
+        lista_noua = os.listdir(ABSOLUTE_PATH_INTRARI)
         for element in lista_noua:
+            #print(element)
             if element not in lista:
                 elements_added = True
-                #print("adaug")
-                break
+                # am luat calea absouta a directorului intrari la care am adaugat si fisierul nou adaugat
+                # apoi am apelat functia add_to_database 
+                file =  ABSOLUTE_PATH_INTRARI + "\\" + element
+                add_to_database(file, myconnection)
+                # mut fisierul in directorul backup_intrari
+                os.replace(file,ABSOLUTE_PATH_BACKUP_INTRARI + '\\' + element)
         if elements_added:
             changed = True
-            print("adaugat")
         else:
             if(len(lista_noua) < len(lista)):
-                #print("sters")
                 changed = True
         if changed: 
-            #print(lista_noua)
             lista = lista_noua
+            print(lista)
         time.sleep(1)
-'2023-05-22T14:23:42.153Z'
-add_to_database("proiect_final\intrari\Poarta1.txt")
-add_to_database("proiect_final\intrari\Poarta2.csv")
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time == ORA_UPDATE:
+            calcul_ore_lucrate(myconnection)
+            
 
-#print(date_to_sql_format('2023-05-22T14:23:42.153Z'))
-# if __name__ == '__main__':
-#     check_for_new_files()      
+        time.sleep(1)
+# add_to_database("proiect_final\intrari\Poarta1.txt",myconnection)
+# add_to_database("proiect_final\intrari\Poarta2.csv",myconnection)
+
+# print(date_to_sql_format('2023-05-22T14:23:42.153Z'))
+if __name__ == '__main__':
+    calcul_ore_lucrate(myconnection)      
 
